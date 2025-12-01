3581032d7635bcd47be76d3f1aefd4ade147cfa7
SHIZUKU_nanoda <1449403026@qq.com>
2025-12-01 09:25:06 +0800

    Merge branch 'master' of https://github.com/LmeSzinc/AzurLaneAutoScript into new

--- module/config/config_updater.py
+++ module/config/config_updater.py
@@ -624,7 +624,7 @@ class ConfigUpdater:
         # 2025.04.17
         # ('Coalition.Coalition.Mode', 'Coalition.Coalition.Mode', coalition_to_frostfall),
         # 2025.06.26
-        ('Coalition.Coalition.Mode', 'Coalition.Coalition.Mode', coalition_to_little_academy),
+        # ('Coalition.Coalition.Mode', 'Coalition.Coalition.Mode', coalition_to_little_academy),
     ]
 
     # redirection += [
@@ -699,7 +699,7 @@ class ConfigUpdater:
         for task in EVENTS + WAR_ARCHIVES:
             default_stage(task, 'D3')
         for task in COALITIONS:
-            default_stage(task, 'hard')
+            default_stage(task, 'area1-normal')
 
         if not is_template:
             new = self.config_redirect(old, new)
